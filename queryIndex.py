eeddb97611f7bc6ac26478a03b0f29cd5dc58b99
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2020-01-28 15:52:24 +0100

    Update queryIndex.py

--- queryIndex.py
+++ queryIndex.py
@@ -1,10 +1,7 @@
 def queryIndex():
     '''O: inverted index from txt file InvertedIndex.txt'''
-    # index from txt file
+    # invertedIndex from txt file
     recInvertedIndex = {}
-
-    # dictionary in a format of inverted index in final form
-    lines = {}
 
     # open invertedIndex txt file
     with open(r'C:\Users\jmsie\Dev\Projects\SearchEngine\search_engine\Include\invertedIndex.txt', 'r') as f:
@@ -13,36 +10,36 @@ def queryIndex():
             line = line.replace('\n', '')
             line = line.split('|')
             # Key = term: Value = id:pos1,pos2,...;
-            lines[line[0]] = line[1]
+            recInvertedIndex[line[0]] = line[1]
 
-        keys = lines.keys()
+        keys = recInvertedIndex.keys()
         # for every term |Key = term: Value = id:pos1,pos2,...;| --> |Key = term: Value = [(id, [pos1, pos2, ...]), ...]
         for key in keys:
 
             # split coordinates by ;
-            lines[key] = lines[key].split(';')
+            recInvertedIndex[key] = recInvertedIndex[key].split(';')
 
             # delete all occurences of whitespace in list of strings
-            lines[key] = [i for i in lines[key] if i != '']
+            recInvertedIndex[key] = [i for i in recInvertedIndex[key] if i != '']
 
             # split id: pos1, pos2, ... by :
-            for string in lines[key]:
-                lines[key][lines[key].index(string)] = tuple(string.split(':'))
+            for string in recInvertedIndex[key]:
+                recInvertedIndex[key][recInvertedIndex[key].index(string)] = tuple(string.split(':'))
             
             # spit positions by , and change each position to int type
-            for coordinates in lines[key]:
+            for coordinates in recInvertedIndex[key]:
                 index, pos = coordinates
+                print(f'coordinates {index}')
                 pos = pos.split(',')
                 for i in pos:
                     pos[pos.index(i)] = int(i)
-                lines[key][lines[key].index(coordinates)] = (index, pos)
-
-    return lines
+                recInvertedIndex[key][recInvertedIndex[key].index(coordinates)] = (int(coordinates[0]), pos)
+    print(f'recInvertedIndex {recInvertedIndex}')
+    return recInvertedIndex
 
 
 def oneWordQuery():
     '''I: query from terminal
-
     O: list of articles' IDs where term occurs
     '''
     # stem searched term
@@ -74,7 +71,6 @@ def oneWordQuery():
 
 def freeTextQuery():
     '''I: Free Text Query from terminal
-
     O: list of articles' IDs where terms occur
     '''   
     from invertedIndex import getTerms
@@ -96,3 +92,6 @@ def freeTextQuery():
 
     print(f'IDs {IDs}')
     return IDs
+
+def phraseQueries():
+    query = input('Search: ')
