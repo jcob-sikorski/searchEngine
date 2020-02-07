b9fc7dc97b3b6862c97b869f30c474f5c1c870ac
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2020-02-07 13:00:36 +0100

    Update invertedIndex.py

--- invertedIndex.py
+++ invertedIndex.py
@@ -4,13 +4,13 @@ from collections import defaultdict
 import pickle
 
 '''
-1) Concatenate the title and the text of the page.
-2) Lowercase all words.
-3) Get all tokens, where a token is a string of alphanumeric characters terminated by a non-alphanumeric character. 
+1) Concatenates the title and the text of the page.
+2) Lowercases all words.
+3) Gets all tokens, where a token is a string of alphanumeric characters terminated by a non-alphanumeric character. 
     The alphanumeric characters are defined to be [a-z0-9]. So, the tokens for the word ‘apple+orange’ would be ‘apple’ 
     and ‘orange’.
-4) Filter out all the tokens that are in the stop words list, such as ‘a’, ‘an’, ‘the’.
-5) Stem each token using Porter Stemmer to finally obtain the stream of terms. 
+4) Filters out all the tokens that are in the stop words list, such as ‘a’, ‘an’, ‘the’.
+5) Stems each token using Porter Stemmer to finally obtain the stream of terms. 
     Porter Stemmer removes common endings from words.
 '''
 
@@ -144,38 +144,21 @@ def createIndex(coll, invertedIndex, termsOnly=True):
                 invertedIndex[word].append((int(articleId[tokens[0]]), tokensPos[word]))
     return invertedIndex
 
-def writeIndexToFile(invertedIndex):
-    # index saved as    term|docID1:pos1,pos2;docID2:pos3,pos4,pos5;…
-    with open(r'C:\Users\jmsie\Dev\Projects\SearchEngine\search_engine\Include\invertedIndex.txt', 'a+') as f:
-        for term in invertedIndex.keys():
-            f.writelines(f'{term}|')
-
-            for ID, position in invertedIndex[term]:
-                # for every articleId write postion of a term in it
-                f.writelines(f'{ID}:')
-                
-                # if there isn't only one position put a comma after it 
-                # or if it's the last position in the list of positions, do not
-                for pos in position:
-                    # if it's the last element in the list do not write a comma
-                    if position.index(pos) == len(position)-1:
-                        f.writelines(f'{pos}')
-                    else:
-                        f.writelines(f'{pos},')
-                f.writelines(f';')
-            f.writelines('\n')
-
 invertedIndex = {}
-coll = []
-# if phrase query termsOnly=False, else True
 
 with open(r'C:\Users\jmsie\Dev\Projects\SearchEngine\search_engine\Include\test.txt', 'r', encoding='utf8') as f:
-    d = f.read().replace('\n', ' ')
+    # collection of articles
+    data = f.read().replace('\n', ' ')
 
-    article = re.findall('<page> (.*?) </page>', d, re.DOTALL)
-    for page in article:
-        createIndex(page, invertedIndex, False)
+    # list of every article in document
+    articles = re.findall('<page> (.*?) </page>', data, re.DOTALL)
 
+    for article in articles:
+        createIndex(article, invertedIndex, False)
+
+# bit container for invertedIndex
 afile = open(r'C:\Users\jmsie\Dev\Projects\SearchEngine\search_engine\Include\idx.txt', 'wb')
+
+# transform invertedIndex to bits
 pickle.dump(invertedIndex, afile)
 afile.close()
