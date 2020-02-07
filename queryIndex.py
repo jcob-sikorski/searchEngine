3ffa5530b51b72469cd63a4845e4a00d7b8006b6
jcob-sikorski <45105669+jcob-sikorski@users.noreply.github.com>
2020-02-07 11:56:58 +0100

    Update queryIndex.py

--- queryIndex.py
+++ queryIndex.py
@@ -93,11 +93,15 @@ def freeTextQuery(invertedIndex):
 
 
 def phraseQueries(invertedIndex):
+    '''I: phraseQuery from terminal with quotes
+    O: list of articles' IDs in which some searched terms occur
+    '''   
     from invertedIndex import getTerms
     #from collections import defaultdict
 
     query = input('Search: ')
     terms = getTerms(query, False)
+    print(f'terms {terms}')
     # IDs of articles in which term occured
     IDs = []
 
@@ -107,42 +111,58 @@ def phraseQueries(invertedIndex):
         if term in invertedIndex.keys():
             for coordinates in invertedIndex[term]:
                 # add all IDs of articles in which the term occurs
-                holder.append(coordinates[0])
-            # add all IDs to the IDs
+                if coordinates[0] not in holder:
+                    holder.append(coordinates[0])
+            # add all IDs provided the previous condition
             IDs.append(holder)
         else:
             print('None!')
             return None
-    print(f'IDs {IDs}')
 
     # IDs of articles in which query occurs
     inter = list(set(intersect(IDs)))
-    positions = {key:[] for key in inter}
+    print(f'inter {inter}')
+    positions = {}
 
-    print(f'\ninter {inter}')
-    #terms = list(set(terms))
+    # add positions of terms to positions
     for term in terms:
         if term in invertedIndex.keys():
             for ID, pos in invertedIndex[term]:
                 if ID in inter:
+                    if ID not in positions.keys():
+                        positions[ID] = []
                     positions[ID].append(pos)
-    
-    print(f'positions {positions}')
+
+    # check if queried words are in the right order
     for ID in inter:
         holder = []
-        for i, termPos in enumerate(positions[ID]):
-            holder.append(termPos[0] - i)
-        positions[ID] = holder
+        for termPos in positions[ID]:
+            for i in termPos:
+                holder.append(i)
+        positions[ID] = sorted(list(set(holder)))
+
+    print(f'positions {positions}')
 
     legit = []
-    print(f'positions {positions}')
+
     for ID in inter:
-        if positions[ID][0] == positions[ID][1] or positions[ID][0] == positions[ID][1]+1:
-            legit.append(ID)
+        #holder is temporary storage for entire sequence of words
+        holder = list(positions[ID])
+        #h is a for a test if query is in particular order
+        h = []
+        print(f'holder {holder}')
+        for ei, elem in enumerate(holder):
+            h.append(elem-ei)
+        print(f'h {h}')
+        #if length of terms is equal to number of same numbers in h, query is in the article
+        for elem in h:
+            if h.count(elem) == len(terms):
+                legit.append(ID)
 
-    print(legit)
+    print(f'legit {list(set(legit))}')
+    return list(set(legit))
 
-    return legit
 
 invertedIndex = queryIndex()
-freeTextQuery(invertedIndex)
+
+phraseQueries(invertedIndex)
